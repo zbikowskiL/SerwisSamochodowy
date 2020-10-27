1ce748575fa097d06996f3afaa49c093a43f44a7
zbikowskiL <lx.zbikowski@gmail.com>
2020-10-27 19:58:47 +0100

    Created own pipe and add pipes to carsList

--- src/app/sharedModule/shared.module.ts
+++ src/app/sharedModule/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { SurnameCutPipe } from './pipe/surname-cut.pipe';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, SurnameCutPipe],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, SurnameCutPipe]
 })
 export class SharedModule { }
