1dcc1c39a1ff69b0c24f36a8d7eba05556e5d1a1
zbikowskiL <lx.zbikowski@gmail.com>
2020-10-27 20:09:44 +0100

    Add own derective

--- src/app/sharedModule/shared.module.ts
+++ src/app/sharedModule/shared.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SurnameCutPipe } from './pipe/surname-cut.pipe';
+import { CsImportantDirective } from './directives/cs-important.directive';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, SurnameCutPipe],
+  declarations: [HeaderComponent, SurnameCutPipe, CsImportantDirective],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, SurnameCutPipe]
+  exports: [HeaderComponent, SurnameCutPipe, CsImportantDirective]
 })
 export class SharedModule { }
