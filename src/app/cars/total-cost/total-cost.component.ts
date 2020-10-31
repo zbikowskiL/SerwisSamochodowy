f4ff88faac0e16b81f511878d2e2e7dba8dc16d7
zbikowskiL <lx.zbikowski@gmail.com>
2020-10-31 18:07:18 +0100

    Add ngOnChanges for total-cost.component

--- src/app/cars/total-cost/total-cost.component.ts
+++ src/app/cars/total-cost/total-cost.component.ts
@@ -1,17 +1,25 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-total-cost',
   templateUrl: './total-cost.component.html',
   styleUrls: ['./total-cost.component.less']
 })
-export class TotalCostComponent {
+export class TotalCostComponent implements OnChanges {
 
   @Input() inputTotalCost: number;
   @Output() shownGross: EventEmitter<number> = new EventEmitter<number>();
   private VAT: number = 1.23;
 
+  costThreshold: number = 10000;
+  isCostToLow: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.isCostToLow = changes['inputTotalCost'].currentValue < this.costThreshold;
+  }
+
   showGross(): void {
     this.shownGross.emit(this.inputTotalCost * this.VAT);
   }
+
 }
