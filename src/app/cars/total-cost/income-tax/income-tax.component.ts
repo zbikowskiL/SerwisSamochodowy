603958af768921ec0683bcd3441a1fad8b12301f
Lukasz Z <lx.zbikowski@gmail.com>
2021-01-07 17:59:42 +0100

    Add Unsubscribe()

--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CostSharedService } from '../../cost-shared.service';
 
 @Component({
   selector: 'app-income-tax',
   templateUrl: './income-tax.component.html',
 })
-export class IncomeTaxComponent implements OnInit {
+export class IncomeTaxComponent implements OnInit, OnDestroy {
 
   private inComeTax : number = 18;
   income : number;
+  costSubscription: Subscription;
 
   constructor(private costSharedService: CostSharedService) { }
-
+  
   ngOnInit(): void {
-    this.costSharedService.totalCostSource$.subscribe((cost) => {
+    this.costSubscription = this.costSharedService.totalCostSource$.subscribe((cost) => {
       this.income = cost * this.inComeTax / 100;
     });
   }
 
+  ngOnDestroy(): void {
+    if(this.costSubscription){
+      this.costSubscription.unsubscribe();
+    }
+  }
 }
